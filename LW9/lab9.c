c11285edd6a65fe3b64c1347b0af4484d274294b
Daniel <daniel.nurgaliev2005@gmail.com>
2023-11-20 22:18:02 +0300

    Выполнена ЛР9

---
+++ LW9/lab9.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int min(int x, int y) {
+    if (x < y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int max(int x, int y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int mod(int a, int b){
+    return a - floor(a / b) * b;
+}
+
+int inArea(double x, double y) {
+    double x1 = -10, y1 = 0;
+    double x2 = 0, y2 = 10;
+    double x3 = -10, y3 = 20;
+
+    double denom = (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3);
+    double u = ((y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)) / denom;
+    double v = ((y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)) / denom;
+    double w = 1 - u - v;
+
+    return (u >= 0 && v >= 0 && w >= 0 && u <= 1 && v <= 1 && w <= 1);
+}
+
+int main() {
+    int i, j, l, k, hit;
+    i = 29;
+    j = -6;
+    l = 1;
+    hit = -1;
+
+    for (k = 1; k <= 50; k++) {
+        i = mod(min(max(min(i - j, i - l), j - l), i - k), 30);
+        j = mod(max(min(max(i - j, i - l), j - l), i - k), 30);
+        l = mod(i, 30) - mod(j, 30) + mod(l, 30) - mod(k, 30);
+
+        if (inArea(i, j)) {
+            hit = k;
+            break;
+        }
+    }
+
+    if (hit != -1) {
+        printf("попал в %d\n", hit);
+    } else {
+        printf("промах\n");
+    }
+
+    printf("конечные: %d %d\n", i, j);
+    printf("динам. параметр: %d\n", l);
+
+    return 0;
+}
