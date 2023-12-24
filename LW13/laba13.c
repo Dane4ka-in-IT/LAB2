82c7a16ba256f46436dbecec8b606983f80e5824
IT-diakon <daniel.nurgaliev2005@gmail.com>
2023-12-24 23:18:56 +0300

    13 лаба

--- LW13/laba13.c
+++ LW13/laba13.c
@@ -2,27 +2,30 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-bool isVowel(char c){
-    char lower_c = tolower(c);
-    return lower_c == 'a' || lower_c == 'e' || lower_c == 'i' || lower_c == 'o' || lower_c == 'u';
+#define VOWELS (1u << ('a' - 'a') | 1u << ('e' - 'a') | 1u << ('i' - 'a') | 1u << ('o' - 'a') | 1u << ('u' - 'a'))
+#define VOICED_CONSONANTS (1u << ('b' - 'a') | 1u << ('d' - 'a') | 1u << ('g' - 'a') | 1u << ('j' - 'a') | 1u << ('l' - 'a') | 1u << ('m' - 'a') | 1u << ('n' - 'a') | 1u << ('r' - 'a') | 1u << ('v' - 'a') | 1u << ('w' - 'a') | 1u << ('z' - 'a'))
+
+bool isVowel(char c) {
+    c = tolower(c);
+    return VOWELS & (1u << (c - 'a'));
 }
 
-bool isConsonant(char c){
-    return isalpha(c) && !isVowel(c);
+bool isConsonant(char c) {
+    return isalpha(c) && !(VOWELS & (1u << (tolower(c) - 'a')));
 }
 
-bool isVoiced(char c){
-    char lower_c = tolower(c);
-    return lower_c == 'b' || lower_c == 'd' || lower_c == 'g' || lower_c == 'j' || lower_c == 'l' || lower_c == 'm' || lower_c == 'n' || lower_c == 'r' || lower_c == 'v' || lower_c == 'w' || lower_c == 'z';
+bool isVoiced(char c) {
+    c = tolower(c);
+    return VOICED_CONSONANTS & (1u << (c - 'a'));
 }
 
-void checkWord(bool word_has_voiced_consonant, bool word_has_unvoiced_consonant, bool *found){
-    if(word_has_voiced_consonant && !word_has_unvoiced_consonant){
+void checkWord(bool word_has_voiced_consonant, bool word_has_unvoiced_consonant, bool *found) {
+    if (word_has_voiced_consonant && !word_has_unvoiced_consonant) {
         *found = true;
     }
 }
 
-int main(){
+int main() {
     bool word_has_unvoiced_consonant = false;
     bool word_has_voiced_consonant = false;
     bool found = false;
@@ -30,16 +33,16 @@ int main(){
 
     printf("words:\n");
 
-    while ((c = getchar()) != EOF){
-        if(isConsonant(c)){
-            if(isVoiced(c)){
+    while ((c = getchar()) != EOF) {
+        if (isConsonant(c)) {
+            if (isVoiced(c)) {
                 word_has_voiced_consonant = true;
             } else {
                 word_has_unvoiced_consonant = true;
             }
         }
 
-        if(c == ' ' || c == '\n' || c == '\t' || c == ','){
+        if (c == ' ' || c == '\n' || c == '\t' || c == ',') {
             checkWord(word_has_voiced_consonant, word_has_unvoiced_consonant, &found);
             word_has_unvoiced_consonant = false;
             word_has_voiced_consonant = false;
@@ -48,11 +51,11 @@ int main(){
 
     checkWord(word_has_voiced_consonant, word_has_unvoiced_consonant, &found);
 
-    if(found) {
+    if (found) {
         printf("yes\n");
     } else {
         printf("no\n");
     }
-    
+
     return 0;
 }
